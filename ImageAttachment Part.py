ac3fb680b0611423b114378aa1c9ab94af93b3b8
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-03-20 09:26:49 +0100

    Made the attachment part not use MIME library.

--- ImageAttachment Part.py
+++ ImageAttachment Part.py
@@ -1,22 +1,22 @@
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
 import base64
 
 
 # https://stackoverflow.com/questions/38360758/base64-encoded-image-in-email
-# Create MIME message.
-def createMIME(imageName=''):
-    msg = MIMEMultipart()
-
-    # Add more attachments if needed
-    #
-    #
-
-    if imageName != '':
-        image = MIMEImage(imageToBase64(imageName), _subtype='octet_stream')
-        image.add_header('Content-Disposition', 'attachment', filename='image.png')
-        msg.attach(image)
+# Create image attachment part to the message
+def createImageAttachment(imageName=''):
+    imageBase64 = imageToBase64(imageName)
+    msg = f'Content-Type: multipart/mixed; boundary="===============0814515963129319972=="\n' \
+          f'MIME-Version: 1.0\n' \
+          f'\n' \
+          f'--===============0814515963129319972==\n' \
+          f'Content-Type: image/octet_stream\n' \
+          f'MIME-Version: 1.0\n' \
+          f'Content-Transfer-Encoding: base64\n' \
+          f'Content-Disposition: attachment; filename="{imageName}"\n' \
+          '\n' \
+          f'{imageBase64}==\n' \
+          '\n' \
+          '--===============0814515963129319972==--' \
 
     return msg
 
@@ -28,6 +28,22 @@ def imageToBase64(imagePath):
 
 
 # Remove when merged
-print(createMIME("image.png").as_string())
+print(createImageAttachment("image.png"))
 
 
+
+# Content-Type: multipart/mixed; boundary="===============0814515963129319972=="
+# MIME-Version: 1.0
+#
+# --===============0814515963129319972==
+# Content-Type: image/octet_stream
+# MIME-Version: 1.0
+# Content-Transfer-Encoding: base64
+# Content-Disposition: attachment; filename="image.png"
+#
+# aVZCT1J3MEtHZ29BQUFBTlNVaEVVZ0FBQUFvQUFBQUxDQUlBQUFESkRJdFBBQUFBQVhOU1IwSUFy
+# czRjNlFBQUFBUm5RVTFCQUFDeGp3djhZUVVBQUFBSmNFaFpjd0FBRW5RQUFCSjBBZDVtSDNnQUFB
+# QW9TVVJCVkNoVFkvaVBGd3dhNlJVclZyUzJ0a0k1WUlBaURaUmpZRUFSb2FMZG1BQ3Y5UC8vQVBR
+# a1FlaXdod1loQUFBQUFFbEZUa1N1UW1DQw==
+#
+# --===============0814515963129319972==--
